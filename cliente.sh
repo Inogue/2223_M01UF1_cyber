29806786c6be2b70dcd277e8e057e443432296cc
Ian Nogueira <ian.nogueira@enti.cat>
2022-11-15 09:48:34 +0100

    Protocolo acabado para 1 archivo

--- cliente.sh
+++ cliente.sh
@@ -1,14 +1,73 @@
 #!/bin/bash
 
+IP_SERVER="localhost"
+IP_LOCAL="127.0.0.1"
+PORT="4242"
+MD5_IP=`echo $IP_LOCAL | md5sum | cut -d " " -f 1`
+
 echo "Cliente HMTP"
 
 echo "(1) ENVIANDO EL SALUDO"
-echo "GREEN_POWA 127.0.0.1" | nc localhost 4242 
+
+echo "GREEN_POWA $IP_LOCAL $MD5_IP" | nc $IP_SERVER $PORT 
+
 echo "(2) RECIBIENDO CONFIRMACION"
-nc -l 4242
+MSG=`nc -l $PORT`
+
 if [ "$MSG" != "OK_HMTP" ]
 then 
-echo "ERROR 1:SALUDO MAL HECHO"
+	echo "ERROR 1:SALUDO MAL HECHO"
 exit 1
 fi
-echo "SIGUE"
+
+FILE_NAME="ElonMusk.jpg"
+echo "(5) ENVIANDO MENSAJE"
+FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
+
+echo "(6) ESCUCHANDO"
+
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_FILENAME" ]
+then
+	echo "ERROR 2: EL NOMBRE DEL ARCHIVO ES ERRONEO"
+	echo "Mensaje de error: $MSG"
+exit 2
+fi
+
+echo "(9) ENVIAMOS DATOS"
+
+cat meme/$FILE_NAME | nc $IP_SERVER $PORT
+
+DATA_MD5=`cat meme/$FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "(10) ESCUCHAMOS RESPUESTA" 
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_DATA_RCPT" ]
+then
+	echo "ERROR 3: DATOS INCORRECTOS"
+	exit 3
+fi
+
+echo "(13) ENVIAMOS CONFIRMACION DE ARCHIVO"
+
+echo $DATA_MD5 | nc $IP_SERVER $PORT
+
+echo "(14) RECIBIMOS CONFIRMACION"
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_DATA_MD5" ]
+then
+	echo "ERROR 4: DATOS FALSOS"
+	echo "MENSAJE DE ERROR: $MSG"
+ 	exit 4
+fi
+
+echo "DATOS ENVIADOS CORRECTAMENTE"
+
+echo "FIN DEL ENVIO"
+exit 0
--- server.sh
+++ server.sh
@@ -1,19 +1,77 @@
 #!/bin/bash
+PORT="4242"
+
 echo "Servidor HMTP" 
 
 echo "(0) LEVANTANDO EL SERVIDOR"
-MSG=`nc -l 4242`
+MSG=`nc -l $PORT`
 
 SALUDO=`echo $MSG | cut -d " " -f 1`
 IP_CLIENTE=`echo $MSG | cut -d " " -f 2`
+MD5_CLIENTE=`echo $MSG | cut -d " " -f 3`
+MD5_IP=`echo $IP_CLIENTE | md5sum | cut -d " " -f 1`
+
 echo "(3) ENVIANDO CONFIRMACION"
+if [ "$MD5_IP" != "$MD5_CLIENTE" ]
+then
+echo "ERROR 1: IP MALA" 
+exit 1
+fi
+
 if [ "$SALUDO" != "GREEN_POWA" ]
 then
-	echo "KO_HMTP" | nc $IP_CLIENTE 4242
+	echo "KO_HMTP" | nc $IP_CLIENTE $PORT
 	exit 1
 fi
-	echo "OK_HTMP" | nc $IP_CLIENTE 4242
-	echo "(4) ESCUCHANDO"
-	nc -l 4242
 
-	exit 0
+echo "OK_HMTP" | nc $IP_CLIENTE $PORT
+
+echo "(4) ESCUCHANDO"
+
+MSG=`nc -l $PORT`
+PREFIX=`echo $MSG | cut -d " " -f 1`
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+FILE_MD5=`echo $MSG | cut -d " " -f 3`
+
+echo "(7) ENVIANDO CONFIRMACION"
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "KO_FILENAME" | nc $IP_CLIENTE $PORT
+	exit 2
+fi
+
+MD5SUM=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+
+if [ "$MD5SUM" != "$FILE_MD5" ]
+then
+	echo "KO_FILE_MD5" | nc $IP_CLIENTE $PORT
+	exit 3
+fi
+
+echo "OK_FILENAME" | nc $IP_CLIENTE $PORT
+
+echo "(8) ESCUCHAMOS ARCHIVO"
+
+nc -l $PORT > inbox/$FILE_NAME
+
+DATA_MD5=`cat inbox/$FILE_NAME | md5sum | cut -d " " -f 1`
+echo "(11) ENVIAR CONFIRMCION" 
+
+echo "OK_DATA_RCPT" | nc $IP_CLIENTE $PORT
+
+echo "(12) ESCUCHANDO CONFIRMACION DE ARCHIVO"
+
+MSG=`nc -l $PORT`
+if [ "$MSG" != "$DATA_MD5" ]
+then
+	echo "KO_DATA_MD5" | nc $IP_CLIENTE $PORT
+	exit 4
+fi
+echo "OK_DATA_MD5" | nc $IP_CLIENTE $PORT
+
+echo "DATOS RECIBIDOS CORRECTAMENTE"
+
+echo "FIN DE LA RECEPCION"
+
+exit 0
